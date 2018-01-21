37f627d4a5dc017840768515cc5cf6a0609252c4
Manuel Pichardo <mlpichardo1@gmail.com>
2018-01-21 23:53:47 +0000

    completed lesson 2

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
+// EXPRESS Level 1
 var express = require('express')
 var app = express();
 
-var requestTime = function (request, response, next) {
-  request.requestTime = Date.now()
-  next()
-}
+// var requestTime = function (request, response, next) {
+//   request.requestTime = Date.now()
+//   next()
+// }
 
-app.use(requestTime)
+// app.use(requestTime)
 
-app.get('/', function (request, response) {
-  response.send('Hello World!')
-})
+// app.get('/', function (request, response) {
+//   response.send('Hello World!')
+// })
 
-app.get('/name', function (request, response) {
-  response.send('Manny Pichardo')
-})
+// app.get('/name', function (request, response) {
+//   response.send('Manny Pichardo')
+// })
 
-app.get('/redirect', function(request, response) {
-  response.redirect(301, '/surprise');
+// app.get('/redirect', function(request, response) {
+//   response.redirect(301, '/surprise');
+// });
+
+// app.listen(process.env.PORT);
+
+// EXPRESS Level 2
+app.get('/cities', function (request, response) {                                                                                                     
+  var cities = ['Providence', 'Warwick', 'Lincoln', 'Cranston'];
+  response.json(cities);
 });
+
+app.use(express.static('public')); //MIDDLEWARE
+
+// app.get("/", function(request, response) {
+//   response.sendFile(__dirname + "/public/index.html");
+// });
 
 app.listen(process.env.PORT);
