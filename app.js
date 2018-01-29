1a51bd86a5548f9550536b8d80d3d58c99681ab3
Manuel Pichardo <mlpichardo1@gmail.com>
2018-01-29 23:09:44 +0000

    updated

--- app.js
+++ app.js
@@ -3,78 +3,10 @@ var app = express();
 var logger = require('./logger');
 app.use(logger);
 
-var bodyParser = require('body-parser');
-var parseUrlencoded = bodyParser.urlencoded({ extended: false });
-
 app.use(express.static('public')); //MIDDLEWARE
 // EXPRESS Level 5
 var blocks = require('./routes/block');
 app.use('/cities', blocks);
 
-var cities = {
-  'Providence': 'RI', 
-  'Bronx': 'NY',
-  'Miami': 'FL',
-  'Oakland': 'CA',
-  'El Paso': 'TX'
-};
-
-app.get('/cities/:name', function (request, response) {
-  var description = cities[request.params.name];
-    if(!description) {
-      response.status(404).json('No description found for ' + request.params.name);
-      console.log(description);
-    } else {
-      response.json(description);
-    }
-});
-
-
-app.get("/cities", function(request, response) {
-  if(request.query.limit >= 0) {
-    response.json(citySearch.slice(0, request.query.limit));
-  } else if (request.query.limit > 5){
-     response.status(404).json
-  } else {
-      response.json(Object.keys(cities));
-  }
-});
-
-function citySearch (keyword) {
-  var regexp = RegExp(keyword, 'i');
-  var result = cities.filter(function(city) {
-    return city.match(regexp);
-  });
-  return result;
-}
-
-// CREATE CITY
-app.post('/cities', parseUrlencoded, function (request, response) {
-  var newCity = request.body;  
-  cities[newCity.name] = newCity.description;
-  
-  response.status(201).json(newCity.name);
-});
-
-var createCity = function(name, description){
-  cities[name] = description;
-  return name; 
-};
-
-// VALIDATION
-app.post('/cities', parseUrlencoded, function (request, response) {
-  if(request.body.description.length > 4){
-    var city = createCity(request.body.name, request.body.description);
-  response.status(201).json(city);
-  } else {
-    response.status(400).json('Invalid City');
-  }
-});
-
-// Delete route
-app.delete('/cities/:name', function (request, response) {
-delete cities[request.cities];
-    response.sendStatus(200);
-}); 
 
 app.listen(process.env.PORT);
--- routes/block.js
+++ routes/block.js
@@ -1,9 +1,63 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var parseUrlencoded = bodyParser.urlencoded({ extended: false });
 
+var cities = {
+  'Providence': 'RI', 
+  'Bronx': 'NY',
+  'Miami': 'FL',
+  'Oakland': 'CA',
+  'Boston': 'MA'
+};
 
+router.route('/')
+
+.get(function(request, response) {
+  if(request.query.limit >= cities.length) {
+    response.status(404).json('Error: limit may not exceed' + cities.length);
+  } if (request.query.limit > 0){
+     response.json(cities.slice(0, request.query.limit));
+  } else {
+      response.json(Object.keys(cities));
+  }
+})
+
+// VALIDATION
+.post(parseUrlencoded, function (request, response) {
+  var newCity = request.body;
+  if (newCity.name.length >= 4 && newCity.description.length >= 2){
+  cities[newCity.name] = newCity.description
+  response.status(201).json(newCity.name);
+  } else {
+    response.status(400).json('Invalid City and State');
+  }
+})
+
+router.route('/:name')
+
+.all(function(request, response, next) {
+  var name = request.params.name;
+  var city = name[0].toUpperCase() + name.slice(1).toLowerCase();
+  request.cityName = city;
+  next();
+})
+
+.get(function(request, response) {
+  var description = cities[request.cityName];
+    if(!description) {
+      response.status(404).json('No description found for ' + request.cityName);
+      console.log(description);
+    } else {
+      response.json(description);
+    }
+})
+
+// Delete route
+.delete(function (request, response) {
+delete cities[request.cityName];
+    response.status(200);
+})
 
 
 module.exports = router;
